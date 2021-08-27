0a87465f6f4a86bf13c887b9649141c74faae654
Scyhthrine <rohangeniusity@gmail.com>
2021-08-28 04:50:56 +0530

    Added bmc

---
+++ pages/_document.tsx
@@ -0,0 +1,31 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+          <script
+            data-name="BMC-Widget"
+            data-cfasync="false"
+            src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+            data-id="manishprivet"
+            data-description="Support me on Buy me a coffee!"
+            data-message=""
+            data-color="#FF813F"
+            data-position="Right"
+            data-x_margin="18"
+            data-y_margin="18"
+          />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
