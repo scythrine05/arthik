3230486f3fc40ac29b14b281fd96abd5555d4a86
Scyhthrine <rohangeniusity@gmail.com>
2021-08-28 04:14:56 +0530

    Added bmc

--- pages/index.js
+++ pages/index.js
@@ -23,6 +23,18 @@ export default function Home(props) {
         </main>
         <Footer />
       </div>
+      <script
+        data-name="BMC-Widget"
+        data-cfasync="false"
+        src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
+        data-id="arthik"
+        data-description="Support me on Buy me a coffee!"
+        data-message=""
+        data-color="#5F7FFF"
+        data-position="Right"
+        data-x_margin="15"
+        data-y_margin="15"
+      ></script>
     </>
   );
 }
